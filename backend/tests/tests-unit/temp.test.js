719cd2f3d382cc0c8035c6f147be4f986c73d097
MMichotte <martin.michotte@gmail.com>
2020-12-26 13:15:47 +0100

    updated tests in CI workflow

---
+++ backend/tests/tests-unit/temp.test.js
@@ -0,0 +1,9 @@
+import chai, { expect } from 'chai'
+import { describe, it } from 'mocha'
+
+describe('dummy test', () => {
+    it('temporary test', () => {
+        expect(true).to.be.true
+        expect(false).to.be.false
+    })
+})
