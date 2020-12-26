20c14a7f9c32125839092bbc3dd44aed52bfd0b5
MMichotte <martin.michotte@gmail.com>
2020-12-26 12:57:57 +0100

    added husky frontend

--- frontend/src/app/spinner/spinner.component.ts
+++ frontend/src/app/spinner/spinner.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./spinner.component.scss']
 })
 export class SpinnerComponent implements OnInit {
-    constructor () { }
+    //constructor () { }
 
     ngOnInit (): void {
     }
