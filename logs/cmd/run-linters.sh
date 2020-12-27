5b8cc1fc6f357814e24b52f2001f85d601ffa49e
MMichotte <martin.michotte@gmail.com>
2020-12-27 12:29:36 +0100

    updated husky config

--- logs/cmd/run-linters.sh
+++ logs/cmd/run-linters.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 printf "\n🔍 Running \e[34mbackend\e[0m linter :\n"
-cd ../backend && npm run lint
+cd ../../backend && npm run lint
 
 echo "-----------------------------"
 
 printf "\n🔍 Running \e[33mfrontend\e[0m linter :\n"
-cd ../frontend && npm run lint
+cd ../../frontend && npm run lint
