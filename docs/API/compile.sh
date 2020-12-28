1174ff2ef3421f229a46f73c1014c03661abc019
MMichotte <martin.michotte@gmail.com>
2020-12-28 10:27:54 +0100

    updated api doc

--- docs/API/compile.sh
+++ docs/API/compile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+
 SWAGGER_FILE="./api-doc.yml"
 OUTPUT_FILE="../../backend/public/api-doc/index.html"
 
