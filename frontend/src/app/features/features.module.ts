d9091ebe2c7d35d94292797baf07f1e67060ebe0
MMichotte <martin.michotte@gmail.com>
2021-02-08 11:17:30 +0100

    created login comp

--- frontend/src/app/features/features.module.ts
+++ frontend/src/app/features/features.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
