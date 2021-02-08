8b3868b8470a3e40ebe94f71e4cd95c8448e267c
MMichotte <martin.michotte@gmail.com>
2021-02-08 12:04:19 +0100

    added user model file

---
+++ backend/database/models/user.js
@@ -0,0 +1,37 @@
+import { Sequelize } from 'sequelize'
+import dbConnection from '../../config/database'
+
+const User = dbConnection.define('user', {
+    id_user: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        unique: false,
+        allowNull: false
+    },
+    role: {
+        type: Sequelize.STRING,
+        unique: false,
+        allowNull: false
+    },
+    createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+    },
+    updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+    }
+})
+
+export default User
