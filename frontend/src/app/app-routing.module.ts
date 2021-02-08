08f835dad29a62d4bdd5e8244a9cfe862e1b4252
MMichotte <martin.michotte@gmail.com>
2021-02-08 11:39:04 +0100

    added login route

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
 import { HomeComponent } from './features/components/home/home.component'
+import { LoginComponent } from './features/components/login/login.component'
 
 const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
 ]
 
 @NgModule({
