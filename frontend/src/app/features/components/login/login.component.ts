bc5edd793ac13e527469208b6fd83187236c1309
MMichotte <martin.michotte@gmail.com>
2021-02-08 15:25:10 +0100

    added login component logic

--- frontend/src/app/features/components/login/login.component.ts
+++ frontend/src/app/features/components/login/login.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../../../core/services/auth.service'
 import { Component, OnInit } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { Router } from '@angular/router'
 
 @Component({
     selector: 'app-login',
@@ -6,8 +9,32 @@ import { Component, OnInit } from '@angular/core'
     styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-    // constructor () { }
+    constructor (private auth: AuthService, private router: Router) { }
+
+    loginForm = new FormGroup({
+        username: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required)
+    })
+
+    displayError = false
 
     ngOnInit (): void {
+    }
+
+    onSubmit () {
+        this.auth.loginUser(this.loginForm.value).subscribe(
+            (res: any) => {
+                localStorage.setItem('token', res.body.token)
+                this.router.navigate(['/'])
+            },
+            error => {
+                this.displayError = true
+                console.log(error.message)
+            }
+        )
+    }
+
+    hideError () {
+        this.displayError = false
     }
 }
