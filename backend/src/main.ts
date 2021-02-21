c76033caa01277e830aeced0d0b806d697eff36c
MMichotte <martin.michotte@gmail.com>
2021-02-21 13:15:55 +0100

    added conf to serve angular app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import env from './config/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  /**
+   * Serve static files from the ../public/dist directory
+   * (angular app will reside in there)
+   */
+  app.useStaticAssets(join(__dirname, '../public/dist'));
+
+  /**
+   * Set base URL to be url/api 
+   */
   app.setGlobalPrefix('api');
 
   /**
@@ -22,6 +34,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('api-docs', app, document);
 
+  /**
+   * Serve application
+   */
   await app.listen(env.PORT);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
