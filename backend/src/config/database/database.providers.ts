7ad3a9214d90e1b3e8a5d25fc08862d1bf6a14ab
MMichotte <martin.michotte@gmail.com>
2021-02-21 14:37:54 +0100

    major dbconfig update

--- backend/src/config/database/database.providers.ts
+++ backend/src/config/database/database.providers.ts
@@ -2,28 +2,88 @@ import { Sequelize } from 'sequelize-typescript';
 import { User } from '../../models/users/entities/user.entity';
 import { SEQUELIZE, DEVELOPMENT, TEST, PRODUCTION } from '../constants';
 import env from '../env';
-import { databaseConfig } from './database.config';
 const consola = require('consola');
 
 export const databaseProviders = [
   {
     provide: SEQUELIZE,
     useFactory: async () => {
-      let config;
+      let sequelize;
       switch (process.env.NODE_ENV) {
       case DEVELOPMENT:
-        config = databaseConfig.dev;
+        sequelize = new Sequelize(
+          env.DB_NAME,
+          env.DB_USER,
+          env.DB_PASSWORD,
+          {
+            host: env.DB_HOST,
+            dialect: 'postgres',
+            logging: false,
+            pool: {
+              max: 5,
+              min: 0,
+              acquire: 30000,
+              idle: 10000
+            }
+          }
+        );
         break;
       case TEST:
-        config = databaseConfig.test;
+        new Sequelize(
+          'slg_db_test',
+          'postgres',
+          'postgres',
+          {
+            host: 'localhost',
+            dialect: 'postgres',
+            logging: false,
+            pool: {
+              max: 5,
+              min: 0,
+              acquire: 30000,
+              idle: 10000
+            },
+            define: {
+              freezeTableName: true
+            }
+          }
+        );
         break;
       case PRODUCTION:
-        config = databaseConfig.prod;
+        sequelize = new Sequelize(
+          env.DATABASE_URL,
+          {
+            ssl: true,
+            dialectOptions: {
+              ssl: {
+                require: true,
+                rejectUnauthorized: false
+              }
+            },
+            define: {
+              freezeTableName: true
+            }
+          });
         break;
       default:
-        config = databaseConfig.dev;
+        sequelize = new Sequelize(
+          env.DB_NAME,
+          env.DB_USER,
+          env.DB_PASSWORD,
+          {
+            host: env.DB_HOST,
+            dialect: 'postgres',
+            logging: false,
+            pool: {
+              max: 5,
+              min: 0,
+              acquire: 30000,
+              idle: 10000
+            }
+          }
+        );
       }
-      const sequelize = new Sequelize(config);
+      
       await sequelize
         .authenticate()
         .then(() => {
@@ -42,7 +102,9 @@ export const databaseProviders = [
         .catch((error) =>
           console.error(`Unable to connect to ${env.DB_NAME} database:`, error),
         );
+
       sequelize.addModels([User]); //! TODO
+
       await sequelize.sync();
       return sequelize;
     },
