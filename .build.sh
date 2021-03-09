2938d631b905baa78d5d5ccbbdacffd673ca9908
MMichotte <martin.michotte@gmail.com>
2021-03-09 11:50:58 +0100

    created new CI

--- .build.sh
+++ .build.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 echo -e "\n\033[32m1. Creating app folder\033[0m"
-mkdir ./www
-mkdir ./www/public
+mkdir ./app
+mkdir ./app/www
+mkdir ./app/www/public
+mkdir ./app/database
 
 echo -e "\n\033[32m2. Building frontend\033[0m"
 cd ./frontend 
 npm i --produdction
 npm run build --produdction
-mv ./dist ../www/public/
+mv ./dist ../app/www/public/
 
 echo -e "\n\033[32m3. Building backend\033[0m"
 cd ../backend
 npm i --produdction
 npm run build --produdction
-cp ./dist ../www
-cp ./.prod.env ../www/.env
-cp ./package.json ../www
-cp ./tsconfig.build.json ../www
+cp -r ./dist ../app/www/
+cp ./package.json ../app/www
+cp ./tsconfig.build.json ../app/www
 
 echo -e "\n\033[42m Done! \033[0m\n"
