66c2ff499881f6781d38f23ad5306b73ce452eb8
MMichotte <martin.michotte@gmail.com>
2021-03-12 14:47:41 +0100

    updated ormconfigs

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -13,7 +13,6 @@ const getMigrationDirectory = () => {
 };
 
 let dbConnectionOptions: ConnectionOptions;
-
 if (env.NODE_ENV === 'dev') {
   consola.info('Connecting to development DB.\n');
   dbConnectionOptions = {
@@ -41,20 +40,10 @@ else if (env.NODE_ENV === 'prod') {
   consola.warn('Connecting to PRODUCTION DB!');
   consola.warn('You have 5s to abort before init continues.\n');
   sleep(5000);
-  let dbUrl: string = env.DATABASE_URL;
-  let ssl: any = {
-    require: true,
-    rejectUnauthorized: false,
-  };
-
-  if (env.RPI_DATABASE_URL){
-    dbUrl = env.RPI_DATABASE_URL;
-    ssl = false;
-  }
   dbConnectionOptions = {
     type: 'postgres',
-    url: dbUrl,
-    ssl: ssl
+    url: env.DATABASE_URL,
+    ssl: false
   };
 }
 
