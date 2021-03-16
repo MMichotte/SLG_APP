ecd96a5bc1b7291f71188dbbeb664196a1c3b72f
MMichotte <martin.michotte@gmail.com>
2021-03-16 17:41:32 +0100

    created client model based on person and company

--- frontend/src/app/features/clients/models/client.model.ts
+++ frontend/src/app/features/clients/models/client.model.ts
@@ -1,13 +1,40 @@
-import { Address } from './../../../core/models/address.model';
-import { ECivility } from '../enums/ECivility.enum';
+import { Address } from '../../../core/models/address.model';
+import { Person } from '../../persons/models/person.model';
+import { Company } from './../../companies/models/company.model';
 
 export class Client {
-  id: number;
-  civility: ECivility;
-  firstName: string;
-  lastName: string;
+
+  type: Person | Company;
+  isCompany: boolean;
+  name: string;
   email: string;
   phone?: string;
   mobile?: string;
   address?: Address;
+  VAT?: string;
+
+  constructor(type: Person | Company) {
+    this.type = type;
+    
+    this.isCompany = (this.type instanceof Company);
+
+    this.name = (this.type instanceof Person)
+      ? `${(<Person>type).lastName} ${(<Person>type).firstName}`
+      : (<Company>type).name;
+
+    this.email = this.type.email;
+
+    this.phone = (this.type instanceof Person)
+      ? (<Person>type).phone
+      : (<Company>type).phone1;
+
+    this.mobile = this.type.mobile; 
+
+    this.address = this.type.address;
+
+    this.VAT = (this.type instanceof Person)
+      ? null
+      : (<Company>type).VAT;
+  }
+
 }
