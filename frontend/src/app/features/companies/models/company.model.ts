ae9120d99ea1ee5d3fd33e7b400105558c6136dd
MMichotte <martin.michotte@gmail.com>
2021-03-16 17:41:01 +0100

    created basic companies module

---
+++ frontend/src/app/features/companies/models/company.model.ts
@@ -0,0 +1,19 @@
+import { ECompanyType } from './../enums/company-type.enum';
+import { Address } from './../../../core/models/address.model';
+
+export class Company {
+  id: number;
+  type: ECompanyType;
+  name: string;
+  email: string;
+  VAT: string;
+  phone1: string;
+  phone2: string;
+  mobile: string;
+  website: string;
+  address: Address;
+
+  constructor(obj?: any) {
+    Object.assign(this, obj);
+  }
+}
