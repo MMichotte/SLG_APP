e45516e5fe1fa7160c325d6bd802d3db468eff31
MMichotte <martin.michotte@gmail.com>
2021-03-19 11:32:50 +0100

    added Error 400

--- frontend/src/app/core/enums/common-errors.enum.ts
+++ frontend/src/app/core/enums/common-errors.enum.ts
@@ -1,3 +1,4 @@
 export enum ECommonErrors {
-  E_50X = 'An unknown error occurred. Retry later.\nIf this error persists, please contact admin.'
+  E_50X = 'An unknown error occurred. Retry later.\nIf this error persists, please contact admin.',
+  E_400 = 'Bad request, please check your form inputs'
 }
