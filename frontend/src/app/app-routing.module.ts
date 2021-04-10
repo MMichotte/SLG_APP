54293837630a6cb60d2485298c53ae84cfac74d7
MMichotte <martin.michotte@gmail.com>
2021-04-10 12:53:20 +0200

    dashboard component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -8,60 +8,65 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/products'
+    redirectTo: '/dashboard'
   },
   {
     path: '',
     children: [
       {
         path: 'login',
-        loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule)
+        loadChildren: () => import('@features/login/login.module').then(m => m.LoginModule)
+      },
+      { 
+        path: 'dashboard',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('@features/dashboard/dashboard.module').then(m => m.DashboardModule)
       },
       { 
         path: 'products',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/products/products.module').then(m => m.ProductsModule)
+        loadChildren: () => import('@features/products/products.module').then(m => m.ProductsModule)
       },
       { 
         path: 'workforce',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/workforces/workforces.module').then(m => m.WorkforcesModule)
+        loadChildren: () => import('@features/workforces/workforces.module').then(m => m.WorkforcesModule)
       },
       { 
         path: 'clients',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/clients/clients.module').then(m => m.ClientsModule)
+        loadChildren: () => import('@features/clients/clients.module').then(m => m.ClientsModule)
       },
       { 
         path: 'cars',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/cars/cars.module').then(m => m.CarsModule)
+        loadChildren: () => import('@features/cars/cars.module').then(m => m.CarsModule)
       },
       { 
         path: 'worksheets',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/worksheets/worksheets.module').then(m => m.WorksheetsModule)
+        loadChildren: () => import('@features/worksheets/worksheets.module').then(m => m.WorksheetsModule)
       },
       { 
         path: 'billing',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/billing/billing.module').then(m => m.BillingModule)
+        loadChildren: () => import('@features/billing/billing.module').then(m => m.BillingModule)
       },
       { 
         path: 'suppliers',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/suppliers/suppliers.module').then(m => m.SuppliersModule)
+        loadChildren: () => import('@features/suppliers/suppliers.module').then(m => m.SuppliersModule)
       },
       { 
         path: 'orders',
         canActivate: [AuthGuard],
-        loadChildren: () => import('./features/orders/orders.module').then(m => m.OrdersModule)
+        loadChildren: () => import('@features/orders/orders.module').then(m => m.OrdersModule)
       },
       { 
         path: 'users',
         canActivate: [AuthGuard],
         data: { role: [EUserRoles.DEV, EUserRoles.ADMIN] },
-        loadChildren: () => import('./features/users/users.module').then(m => m.UsersModule)
+        loadChildren: () => import('@features/users/users.module').then(m => m.UsersModule)
       }
     ] 
   }
---
+++ frontend/src/app/features/dashboard/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['../../../../shared/styles/style.scss', './dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+  }
+
+}
